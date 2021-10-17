887c5af80d3c15531439d4bb6c52030ac859c4ce
yama-choki <lovepingpong0@gmail.com>
2021-10-17 21:17:26 +0900

    大まかな見た目の作成

---
+++ components/Header.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-app-bar
+    app
+    color="#fcb69f"
+    dark
+    max-width="1000px"
+  >
+    <template #img="{ props }">
+      <v-img
+        v-bind="props"
+        gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+      />
+    </template>
+
+    <v-app-bar-nav-icon />
+
+    <v-toolbar-title>やることリスト</v-toolbar-title>
+
+    <v-spacer />
+    <v-dialog
+      v-model="addTodoDialog"
+      max-width="600"
+      persistent
+    >
+      <template #activator="{ on, attrs }">
+        <v-btn
+          icon
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-icon>mdi-plus-circle-outline</v-icon>
+        </v-btn>
+      </template>
+      <template #default="dialog">
+        <v-card>
+          <v-toolbar
+            color="primary"
+            dark
+          >
+            やることを追加する
+          </v-toolbar>
+          <div class="mx-5">
+            <v-text-field v-model="todo.title" label="やること" class="mt-5" counter="10" />
+            <v-text-field v-model="todo.text" label="やることの説明" class="mt-5" counter="50" />
+            <h3 class="my-5 limit-title">
+              期限を入力してください
+            </h3>
+            <v-row justify="center">
+              <v-date-picker v-model="todo.limit" />
+            </v-row>
+          </div>
+          <v-card-actions class="justify-end">
+            <v-btn text color="primary" @click="addTodo()">
+              追加する
+            </v-btn>
+            <v-btn text color="primary" @click="dialog.value = false">
+              キャンセル
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </template>
+    </v-dialog>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      addTodoDialog: false,
+      deleteTodoDialog: false,
+      todo: ''
+    }
+  }
+}
+</script>
+
+<style>
+.limit-title{
+  display: flex;
+  justify-content: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,26 @@
 <template>
-  <Tutorial />
+  <v-app>
+    <div id="body">
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Todos />
+      </main>
+    </div>
+  </v-app>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style>
+main{
+  max-width: 1000px;
+  margin: 0 auto;
+}
+header{
+  margin: 0 auto;
+}
+</style>
